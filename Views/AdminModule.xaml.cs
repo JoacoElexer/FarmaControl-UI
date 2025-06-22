662b9f65fd89391577c859ae48eb3e7df72a9a5d
Joaco_Vrz <joacoe.vrz@gmail.com>
2025-06-22 00:27:15 -0600

    Assets añadidos | MainPage como base creada | Estructura para interfaz de modulo de login lista (Sin estilos)

---
+++ Views/AdminModule.xaml.cs
@@ -0,0 +1,9 @@
+namespace FarmaControl_UI.Views;
+
+public partial class AdminModule : ContentView
+{
+	public AdminModule()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/LoginModule.xaml.cs
@@ -0,0 +1,9 @@
+namespace FarmaControl_UI.Views;
+
+public partial class LoginModule : ContentView
+{
+	public LoginModule()
+	{
+		InitializeComponent();
+	}
+}
